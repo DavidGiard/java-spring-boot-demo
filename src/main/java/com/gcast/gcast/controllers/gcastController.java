6b6c25b4df83c4f71da77b7d5bdc9e467868e9f6
david.giard.osv <david.giard.osv@fedex.com>
2020-07-25 19:45:12 -0500

    Get and set values in HTTP header request and response

--- src/main/java/com/gcast/gcast/controllers/gcastController.java
+++ src/main/java/com/gcast/gcast/controllers/gcastController.java
@@ -2,6 +2,8 @@ package com.gcast.gcast.controllers;
 
 import java.util.UUID;
 
+import javax.servlet.http.HttpServletRequest;
+
 import com.gcast.gcast.models.GreetingInput;
 import com.gcast.gcast.models.GreetingOutput;
 import com.gcast.gcast.services.MathService;
@@ -11,6 +13,7 @@ import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -56,16 +59,24 @@ public class gcastController {
     @GetMapping("AddNumbers/{firstNumber}/{secondNumber}")
     public ResponseEntity<Integer> AddNumbers(
             @PathVariable("firstNumber") Integer firstNumber,
-            @PathVariable("secondNumber") Integer secondNumber
+            @PathVariable("secondNumber") Integer secondNumber,
+            HttpServletRequest request
     ) {
 
-        String requestKey = UUID.randomUUID().toString();
+        String requestKey = request.getHeader("X-Request-Key");
+        if (requestKey == null){
+            requestKey = UUID.randomUUID().toString();
+        }
+
         MDC.put("RequestKey", requestKey);
 
         logger.info("Controller started. AddNumbers");
         logger.info("First: " + firstNumber);
         logger.info("Second: " + secondNumber);
         Integer sum = mathService.AddNumbers(firstNumber, secondNumber);
-        return new ResponseEntity<Integer>(sum, HttpStatus.OK);
+        
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.set("X-Request-Key", requestKey);
+        return new ResponseEntity<Integer>(sum, responseHeaders, HttpStatus.OK);
     }
 }
